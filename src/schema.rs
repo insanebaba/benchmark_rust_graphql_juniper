4505b995300ac569e5bc47f3035240bb2cc357e9
vikram mishra <vikram.mishra420@gmail.com>
2020-01-15 14:47:20 +0100

    Update schema.rs
    remove useless objects

--- src/schema.rs
+++ src/schema.rs
@@ -2,13 +2,6 @@ use fake::{Dummy, Fake, Faker};
 use juniper::FieldResult;
 use juniper::RootNode;
 
-#[derive(GraphQLEnum)]
-enum Episode {
-    NewHope,
-    Empire,
-    Jedi,
-}
-
 #[derive(GraphQLObject, Debug, Dummy)]
 #[graphql(description = "An author")]
 struct Author {
@@ -28,7 +21,7 @@ struct Books {
 
 fn gen_data() -> Vec<Author> {
     let mut result: Vec<Author> = Vec::new();
-    for _ in 0..21 {
+    for _ in 0..=20 {
         let mut auth = Faker.fake::<Author>();
         for _ in 0..4 {
             auth.books.push(Faker.fake::<Books>());
@@ -38,34 +31,9 @@ fn gen_data() -> Vec<Author> {
     result
 }
 
-#[derive(GraphQLObject)]
-#[graphql(description = "A humanoid creature in the Star Wars universe")]
-struct Human {
-    id: String,
-    name: String,
-    appears_in: Vec<Episode>,
-    home_planet: String,
-}
-
-#[derive(GraphQLInputObject)]
-#[graphql(description = "A humanoid creature in the Star Wars universe")]
-struct NewHuman {
-    name: String,
-    appears_in: Vec<Episode>,
-    home_planet: String,
-}
-
 pub struct QueryRoot;
 
 graphql_object!(QueryRoot: () |&self| {
-    field human(&executor, id: String) -> FieldResult<Human> {
-        Ok(Human{
-            id: "1234".to_owned(),
-            name: "Luke".to_owned(),
-            appears_in: vec![Episode::NewHope],
-            home_planet: "Mars".to_owned(),
-        })
-    },
     field authors(&executor) -> FieldResult<Vec<Author>> {
         Ok(gen_data())
     }
@@ -74,14 +42,6 @@ graphql_object!(QueryRoot: () |&self| {
 pub struct MutationRoot;
 
 graphql_object!(MutationRoot: () |&self| {
-    field createHuman(&executor, new_human: NewHuman) -> FieldResult<Human> {
-        Ok(Human{
-            id: "1234".to_owned(),
-            name: new_human.name,
-            appears_in: new_human.appears_in,
-            home_planet: new_human.home_planet,
-        })
-    }
 });
 
 pub type Schema = RootNode<'static, QueryRoot, MutationRoot>;
